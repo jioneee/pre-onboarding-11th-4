e8f384ddd94b029af913d176622b04df99f5301d
jioneee <qoxx3659@gmail.com>
2023-07-18 11:45:47 +0900

    feat: api data 불러오기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'></header>
-      <Main></Main>
+      <Main />
     </div>
   );
 }
--- client/src/api/sickApi.tsx
+++ client/src/api/sickApi.tsx
@@ -5,7 +5,7 @@ export function getSick() {
     url: 'http://localhost:4000/sick'
   })
     .then(function (res) {
-      console.log('res', res);
+      console.log('res', res.data);
     })
     .catch(function (error) {
       console.log('error', error);
